f68866595d961e0f40f863fce0731087731e3517
Benny Mei <bmei3@binghamton.edu>
2015-04-11 11:23:37 -0400

    added additional checks for correct input for generate_numbers.cpp

--- generate_numbers.cpp
+++ generate_numbers.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <sstream>
 
 /* This file is used to generate input for the sorting algorithms
  * that we propose to write.
@@ -10,13 +11,14 @@
  * ./generate_inputs <size_of_input> <state> <duplicates> <output_file>
  * 
  * where size_of_input represents how large of a dataset should be generated
- * state represents whether the data is sorted, unsorted, randomized
+ * state represents whether the data is sorted, reversely sorted, randomized
  * duplicates is a flag determining if duplicates are allowed in the set
  * output_file is a file where the dataset should be written
  * 
 */
 
-bool correctArgumentFormat(int argc, char* argv[]);
+
+bool correctArgumentFormat(int argc, char *argv[]);
 
 int main(int argc, char* argv[]){
   
@@ -32,7 +34,7 @@ int main(int argc, char* argv[]){
   return 0;
 }
 
-bool correctArgumentFormat(int argc, char* argv[]){
+bool correctArgumentFormat(int argc, char *argv[]){
   int NUM_CORRECT_ARGUMENTS = 5;
   std::string USAGE_MESSAGE = "Usage: <size_of_input> <state> <duplicates> <output_file>";
   if(argc != NUM_CORRECT_ARGUMENTS){
@@ -40,5 +42,35 @@ bool correctArgumentFormat(int argc, char* argv[]){
     std::cout<<USAGE_MESSAGE<<std::endl;
     return false;
   }
+  
+  std::stringstream ss;
+
+  //Check whether the first argument is an integer
+  ss << argv[1];
+  int inputSize; 
+  ss >> inputSize;
+  if(ss.fail()){
+    std::cout<<"Error. size_of_input must be an integer."<<std::endl;
+    std::cout<<USAGE_MESSAGE<<std::endl;
+    return false;
+  }
+  ss.clear();
+  std::string stateString;
+  ss << argv[2];
+  ss >> stateString;
+  if(ss.fail()){
+    std::cout<<"Error. state must be a string."<<std::endl;
+    std::cout<<USAGE_MESSAGE<<std::endl;
+    return false;
+  }
+  ss.clear();
+  if(!(stateString == "sorted" || 
+     stateString == "reversely_sorted" || 
+     stateString == "randomized")){
+    std::cout<<"Error. state must be either \"sorted\", \"reversely_sorted\", or \"randomized\"."<<std::endl;
+    return false;
+  }
+
+
   return true;
 }
