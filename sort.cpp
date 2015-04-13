30349d6965c0d9032cb736000c41a3a08c92deba
Benny Mei <meibenny@gmail.com>
2015-04-12 22:09:56 -0400

    added sort.cpp

---
+++ sort.cpp
@@ -0,0 +1,11 @@
+/* Use this file to sort the automatically generated input
+ *
+ * NOTE: This is not the implementation of the project code
+ * This file merely sorts the input so that we can be sure that
+ * the code we write for our project is sorting correctly
+*/
+
+
+int main(int argc, char* argv[]){
+  return 0;
+}
