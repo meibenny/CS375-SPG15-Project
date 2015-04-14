a226b7a87aac2bd7d6e2f9478d89fbf8104ff8db
Benny Mei <meibenny@gmail.com>
2015-04-14 16:26:25 -0400

    implemented quicksort with lomuto parittioning. looks like it works...

--- quicksort_lomuto.cpp
+++ quicksort_lomuto.cpp
@@ -6,7 +6,11 @@
 #include <vector>
 using namespace std;
 
+void quicksort(std::vector<int> &A, int p, int r);
+int partition(std::vector<int> &A, int p, int r);
+
 int main(int argc, char* argv[]){
+  bool debug = !true;
   if(!correctArgumentFormat(argc, argv))
   {
     exit(1);
@@ -20,6 +24,11 @@ int main(int argc, char* argv[]){
     exit(1);
   }
   //implement quicksort
+  int array_first = 0;
+  int array_last  = (int) numbers.size()-1;
+  if(debug){for(auto e: numbers){cout<<e<<endl;}}
+  quicksort(numbers, array_first, array_last);
+  if(debug){for(auto e: numbers){cout<<e<<endl;}}
   string output_file = argv[2];
   success = outputToFile(output_file, numbers);
   if(success != 0)
@@ -28,4 +37,33 @@ int main(int argc, char* argv[]){
     exit(1);
   }
   return 0;
+}
+
+void quicksort(std::vector<int> &A, int p, int r){
+  bool debug = !true;
+  if(debug){cout<<"p: "<<p<<" r: "<<r<<endl;}
+  int q;
+  if(p < r)
+  {
+    q = partition(A, p, r);
+    quicksort(A, p, q-1);
+    quicksort(A, q+1, r);
+  }
+}
+
+int partition(std::vector<int> &A, int p, int r){
+  bool debug = !true;
+  int x = A[r];
+  int i = p-1;
+  if(debug){cout<<"x: "<<x<<" i: "<<i<<endl;}
+  for(int j = p; j < r; j++)
+  {
+    if(A[j] <= x)
+    {
+      i = i+1;
+      swap(A[i],A[j]);
+    }
+  }
+  swap(A[i+1],A[r]);
+  return i+1;
 }
