eaeb872fd68593d38ce7dd91147cf1a5c83d6d31
Benny Mei <meibenny@gmail.com>
2015-04-14 17:49:22 -0400

    Tried to implement hoare partionining. A lot of the code is here, but it still segfaults.

--- quicksort_hoare.cpp
+++ quicksort_hoare.cpp
@@ -6,7 +6,11 @@
 #include <vector>
 using namespace std;
 
+void quicksort(std::vector<int> &A, int p, int r);
+int partition(std::vector<int> &A, int p, int r);
+
 int main(int argc, char* argv[]){
+  bool debug = !true;
   if(!correctArgumentFormat(argc, argv))
   {
     exit(1);
@@ -20,6 +24,11 @@ int main(int argc, char* argv[]){
     exit(1);
   }
   //implement quicksort
+  int array_first = 0;
+  int array_last  = (int) numbers.size()-1;
+  if(debug){for(auto e: numbers){cout<<e<<endl;}}
+  quicksort(numbers, array_first, array_last);
+  if(debug){for(auto e: numbers){cout<<e<<endl;}}
   string output_file = argv[2];
   success = outputToFile(output_file, numbers);
   if(success != 0)
@@ -28,4 +37,55 @@ int main(int argc, char* argv[]){
     exit(1);
   }
   return 0;
+}
+
+void quicksort(std::vector<int> &A, int p, int r){
+  bool debug = !true;
+  if(debug){cout<<"p: "<<p<<" r: "<<r<<endl;}
+  int q;
+  if(r - p < 2)
+  {
+    return;
+  }
+  q = partition(A, p, r);
+  quicksort(A, p, q);
+  quicksort(A, q, r);
+}
+
+int partition(std::vector<int> &A, int p, int r){
+  bool debug = true;
+  if(debug){cout<<"in partition"<<endl;}
+  if(debug)
+  {
+    cout<<"segfault when assigning x. p: "<<p<<endl;
+  }
+  int x = A[p];
+  int i = p - 1;
+  int j = r + 1;
+  while (true)
+  {
+    do
+    {
+      j = j - 1;
+    }
+    while(A[j] > x);
+    
+    do
+    {
+      i = i + 1;
+    }
+    while(A[i] < x);
+    
+    if(i < j)
+    {
+      if(debug){cout<<"segfault when swapping."<<endl;}
+      if(debug){cout<<"i: "<<i<<endl;}
+      if(debug){cout<<"j: "<<j<<endl;}
+      swap(A[i], A[j]);
+    }
+    else
+    {
+      return j;
+    }
+  }
 }
