519564cca9ffdc4be5e6690b87c92cf2f3e8c9f2
Benny Mei <meibenny@gmail.com>
2015-04-14 14:58:33 -0400

    fixed some braces and added code to read input files

--- auxiliary_functions.cpp
+++ auxiliary_functions.cpp
@@ -4,17 +4,53 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <vector>
 
 using namespace std;
 
+/* Returns true if the arguments given to program are correct.
+ * Returns false is arguments are incorrect.
+ * Arguments should be <input_file> <output_file>
+ * Where <input_file> is a file containing numbers to be sorted.
+ * Where <output_file> is a file where the sorted output should be placed.
+*/
 bool correctArgumentFormat(int argc, char* argv[]);
 
+/* Can be called by sort functions to read the input file.
+ * Stores input in int vector, buffer.
+ * returns 0 if operation completes successfully.
+ * returns 1 if operation fails for any reason
+ * Argument should be <input_file> <buffer>
+ * Where <input_file> contains the numbers to be sorted.
+ * Where <buffer> is an empty int vector
+ * readInputFile modifies <buffer> and stores the input inside <buffer>
+ *
+*/
+int readInputFile(string file_name, std::vector<int>&buffer);
+
+int readInputFile(string file_name, std::vector<int>& buffer){
+  string input_file = file_name;
+  ifstream input;
+  input.open(input_file); 
+  
+  //input_file failed to open for any reason
+  if(!input.is_open()){return 1;}
+
+  int eachNumber;
+  while(input >> eachNumber)
+  {
+    buffer.push_back(eachNumber);
+  }
+  input.close();
+  return 0;
+}
 
 bool correctArgumentFormat(int argc, char* argv[]){
   int NUM_CORRECT_ARGUMENTS = 3;
   string USAGE_MESSAGE = "USAGE: <input-file> <output-file>"; 
 
-  if(argc != NUM_CORRECT_ARGUMENTS){
+  if(argc != NUM_CORRECT_ARGUMENTS)
+  {
     cout<<"Expected "<<NUM_CORRECT_ARGUMENTS<<". Received "<<argc<<" arguments."<< endl;
     cout << USAGE_MESSAGE << endl;
     return false;
@@ -24,13 +60,15 @@ bool correctArgumentFormat(int argc, char* argv[]){
   ss << argv[1];
   string inputFile;
   ss >> inputFile;
-  if(ss.fail()){
+  if(ss.fail())
+  {
     cout << "Error. input-file must be a string." << endl;
     return false;
   }
   ifstream input;
   input.open(inputFile);
-  if(!input.is_open()){
+  if(!input.is_open())
+  {
     cout << "Error. input-file must exist." << endl;
     return false;
   }
@@ -38,7 +76,8 @@ bool correctArgumentFormat(int argc, char* argv[]){
   ss << argv[2];
   string outputFile;
   ss >> outputFile;
-  if(ss.fail()){
+  if(ss.fail())
+  {
     cout << "Error. output-file must be a string." << endl;
     return false;
   }
