efaa466d01bc9744c0b277cf334ce03552e92a42
David Skoda <Dskoda1@binghamton.edu>
2015-04-29 14:10:32 -0400

    first prototype of input analyzation

--- hybrid.cpp
+++ hybrid.cpp
@@ -4,14 +4,18 @@
  *
  *
  */
-#include "timer.cpp"
 #include "auxiliary_functions.cpp"
+#include "timer.cpp"
 #include <vector>
 #include <climits>
+#include <set>
 using namespace std;
 
 
-
+//Function to analyze whether or not input
+//contains a high number of duplicates
+//Returns 1 if high number, 0 if low
+int analyzeInput(vector<int>& a);
 
 int main(int argc, char ** argv)
 {
@@ -40,11 +44,16 @@ int main(int argc, char ** argv)
 	struct timeval end[numRuns];
 
 	vector<float> time;
+	int dup = 0;
 	for(int i = 0; i < numRuns; i++)
 	{
 		numbers.clear();
 		numbers.assign(holder.begin(), holder.end());
 		start[i] = startTime(&start[i]);
+
+		dup = analyzeInput(numbers);
+
+
 		//would hybrid sort here.. IF I HAD ONE
 		end[i] = endTime(&end[i]);
 		time.push_back(calculateTime(&start[i], &end[i]));
@@ -54,7 +63,14 @@ int main(int argc, char ** argv)
 	 *  End timing sequence of:
 	 *  Hybrid sort
 	 */
-
+	if(dup)
+	{
+		cout << "High number of duplicates detected." << endl;
+	}
+	else
+	{
+		cout << "Low number of duplicated detected." << endl;
+	}
 	string output_file = argv[2];
 	success = outputToFile(output_file, numbers);
 	if(success != 0)
@@ -64,3 +80,33 @@ int main(int argc, char ** argv)
 	}
 	return 0;
 }
+
+
+int analyzeInput(vector<int>& a)
+{
+
+	int dupVal = 0;
+	int duplicates = 0;
+	int lastVal = 0;
+	lastVal = a[0];
+	set<int> values;
+	values.insert(lastVal);	
+	for(int i = 0 ; i <(int) a.size(); i += 20)
+	{
+		if(values.find(a[i]) == values.end())
+		{
+			values.insert(a[i]);
+			if(values.size() > 15)
+				break;
+		}	
+		
+	}
+
+	if(values.size() < 15)
+		dupVal = 1;
+
+
+	return dupVal;
+
+}
+
