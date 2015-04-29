786c5fa3dd73578798d1ab99c006a4fb9808d3af
David Skoda <Dskoda1@binghamton.edu>
2015-04-29 11:50:24 -0400

    Shell script for testing

---
+++ newTests.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+./quicksort_median tests/testfiles/1_r-sorted_true.txt out.txt
+./quicksort_median tests/testfiles/2_r-sorted_true.txt out.txt
+./quicksort_median tests/testfiles/16_r-sorted_true.txt out.txt
+./quicksort_median tests/testfiles/32_r-sorted_true.txt out.txt
+./quicksort_median tests/testfiles/128_r-sorted_true.txt out.txt
+./quicksort_median tests/testfiles/512_r-sorted_true.txt out.txt
+./quicksort_median tests/testfiles/1024_r-sorted_true.txt out.txt
+./quicksort_median tests/testfiles/4096_r-sorted_true.txt out.txt
+./quicksort_median tests/testfiles/16348_r-sorted_true.txt out.txt
+./quicksort_median tests/testfiles/65536_r-sorted_true.txt out.txt
+./quicksort_median tests/testfiles/524288_r-sorted_true.txt out.txt
+
+
+
+
+
