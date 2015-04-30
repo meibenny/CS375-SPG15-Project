6d2d5c8c66b654864c9438bde675c754dce725b8
Benny Mei <meibenny@gmail.com>
2015-04-29 20:16:13 -0400

    added heapsort code

--- introsort.cpp
+++ introsort.cpp
@@ -2,6 +2,7 @@
 #include "timer.cpp"
 #include <iostream>
 #include <vector>
+#include <cmath>
 
 using namespace std;
 
@@ -9,6 +10,9 @@ using namespace std;
 void sort(std::vector<int> &A, int p, int r);
 void introsort(std::vector<int> &A, int p, int r, int max_depth);
 int partition(std::vector<int> &A, int p, int r);
+void heapsort(vector<int> &arr, int numEle);
+void maxHeapify(vector<int>& arr, int index, int numEle);
+void buildMaxHeap(vector<int> &arr, int numEle);
 
 int main(int argc, char*[] argv)
 {
@@ -67,6 +71,25 @@ int main(int argc, char*[] argv)
   return 0;
 }
 
+void sort(std::vector<int> &A, int p, int r)
+{
+  //We use log10 because the algorithm doens't specify which log to use.
+  int max_depth = floor(log10(A.length())) * 2;
+  introsort(A, p, r, max_depth);
+}
+
+void introsort(std::vector<int> &A, int p, int r, int max_depth)
+{
+  int n = A.length();
+  if(n <= 1)
+  {
+    return; //We're in the base case
+  }
+  else if(max_depth = 0)
+  {
+    heapsort(A);
+  }
+}
 int partition(std::vector<int> &A, int p, int r){
 	int x = A[p];
 	int i = p - 1;
@@ -93,5 +116,43 @@ int partition(std::vector<int> &A, int p, int r){
 		{
 			return j;
 		}
+	}
+}
+
+
+void maxHeapify(vector<int>& arr, int index, int numEle){
+	int left = 2*index;
+	int right = (2*index)+1;
+	int largest = index;
+	if(left <= numEle && arr[left] > arr[index]){
+		largest = left;
+	}
+	else{
+		largest = index;
+	}
+	if(right <= numEle && arr[right] > arr[largest]){
+		largest = right;
+	}
+	if(largest != index){
+		int temp = arr[index];
+		arr[index] = arr[largest];
+		arr[largest] = temp;
+		maxHeapify(arr, largest, numEle);
+	}
+}
+
+void buildMaxHeap(vector<int> &arr, int numEle){
+	for(int i = floor(numEle/2); i>=1; i--){
+		maxHeapify(arr,i,numEle);
+	}
+}
+
+void heapsort(vector<int> &arr, int numEle){
+	buildMaxHeap(arr , numEle);
+	for(int i = numEle; i >=2; i--){
+		long temp = arr[1];
+		arr[1] = arr[i];
+		arr[i] = temp;
+		maxHeapify(arr, 1, i-1);
 	}
 }
