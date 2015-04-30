aa1124d00231b97d6fb85e933ad78577d704c815
David Skoda <Dskoda1@binghamton.edu>
2015-04-29 22:23:18 -0400

    Hybrid sort in progress

--- hybrid.cpp
+++ hybrid.cpp
@@ -1,7 +1,7 @@
 #include "hybrid.h"
 using namespace std;
 void sort(vector<int> & A, int p, int r);
-void introsort(vector<int> &A, int p, int r, int max_depth);
+void hoareSort(vector<int> &A, int p, int r, int max_depth);
 
 int main(int argc, char ** argv)
 {
@@ -38,7 +38,16 @@ int main(int argc, char ** argv)
 		start[i] = startTime(&start[i]);
 
 		dup = analyzeInput(numbers);
-		sort(numbers, 0, (int)numbers.size()-1);
+		if(dup)
+		{
+			sort(numbers, 0, (int)numbers.size()-1);
+			
+			cout << "High number of duplicates detected." << endl;
+		}
+		else
+		{
+			cout << "Low number of duplicated detected." << endl;
+		}
 
 		end[i] = endTime(&end[i]);
 		time.push_back(calculateTime(&start[i], &end[i]));
@@ -48,14 +57,6 @@ int main(int argc, char ** argv)
 	 *  End timing sequence of:
 	 *  Hybrid sort
 	 */
-	if(dup)
-	{
-		cout << "High number of duplicates detected." << endl;
-	}
-	else
-	{
-		cout << "Low number of duplicated detected." << endl;
-	}
 	string output_file = argv[2];
 	success = outputToFile(output_file, numbers);
 	if(success != 0)
@@ -68,30 +69,30 @@ int main(int argc, char ** argv)
 
 void sort(std::vector<int> &A, int p, int r)
 {
-  //This depth will be fine tuned after running tests
-  int max_depth = 2* floor(log2(A.size()));
-  introsort(A, p, r, max_depth);
+	//This depth will be fine tuned after running tests
+	int max_depth = 2* floor(log2(A.size()));
+	hoareSort(A, p, r, max_depth);
 }
 
-void introsort(vector<int> &A, int p, int r, int max_depth)
+void hoareSort(vector<int> &A, int p, int r, int max_depth)
 {
-  int n = r-p;
-  if(n <= 1)
-  {
-    return; //We're in the base case
-  }
-  else if(max_depth == 0)
-  {
-    heapsort(A, n);
-    return;
-  }
-  else
-  {
-    int q;
-    q = partitionHoare(A, p, r);
-    introsort(A, p, q, --max_depth);
-    introsort(A, q+1, r, --max_depth);
-  }
+	int n = r-p;
+	if(n <= 1)
+	{
+		return; //We're in the base case
+	}
+	else if(max_depth == 0)
+	{
+		heapsort(A, n);
+		return;
+	}
+	else
+	{
+		int q;
+		q = partitionHoare(A, p, r);
+		hoareSort(A, p, q, --max_depth);
+		hoareSort(A, q+1, r, --max_depth);
+	}
 
 
 }
